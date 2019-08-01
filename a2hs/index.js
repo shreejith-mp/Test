f624d991ce9744257b0bcbfb8422b706f81e0af3
shreejith-mp <49388988+shreejith-mp@users.noreply.github.com>
2019-08-01 16:18:58 +0530

    Update index.js

--- a2hs/index.js
+++ a2hs/index.js
@@ -29,7 +29,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
     // Show the prompt
-    deferredPrompt.prompt();
+    e.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
