04fe2d7d65d5c359140f7d2aceff5ee69db68b1a
shreejith-mp <49388988+shreejith-mp@users.noreply.github.com>
2019-08-01 18:50:11 +0530

    Update sw.js

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -15,28 +15,11 @@ self.addEventListener('install', function(e) {
  );
 });
 
-addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;     // if valid response is found in cache return it
-        } else {
-          return fetch(event.request)     //fetch from internet
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());    //save the response for future
-                  return res;   // return the fetched data
-                })
-            })
-            .catch(function(err) {       // fallback mechanism
-              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                .then(function(cache) {
-                  return cache.match('/offline.html');
-                });
-            });
-        }
-      })
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
-}); 
+});
