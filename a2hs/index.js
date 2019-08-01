529356d5acf9c735c5716c4976e8e612038704ab
shreejith-mp <49388988+shreejith-mp@users.noreply.github.com>
2019-08-01 16:10:49 +0530

    Update index.js

--- a2hs/index.js
+++ a2hs/index.js
@@ -11,6 +11,13 @@ setInterval(function() {
   imgElem.src = 'images/' + randomChoice + '.jpg';
 }, 2000)
 
+// Register service worker to control making site work offline
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('sw.js')
+           .then(function() { console.log('Service Worker Registered'); });
+}
 
 // Code to handle install prompt on desktop
 
